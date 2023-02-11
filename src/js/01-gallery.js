09be31057aa0445fa5a4afb544a698628c987fc7
Your Name <pavlo.morozenko@ukr.net>
2023-02-11 18:57:38 +0100

    task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,26 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+const gallerySection = document.querySelector('.gallery');
+const galleryMarkUp = createGalleryMarkUp(galleryItems);
+
+gallerySection.insertAdjacentHTML('beforeend', galleryMarkUp);
+
+function createGalleryMarkUp(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+            <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`;
+    })
+    .join('');
+}
+
+var lightbox = new SimpleLightbox('.gallery__item', {
+  captionDelay: 250,
+  captionData: 'alt',
+});
