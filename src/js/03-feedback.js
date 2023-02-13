ecc728c4634916b3263821750d2813cc315a9203
Your Name <pavlo.morozenko@ukr.net>
2023-02-13 15:39:53 +0100

    task-3/1

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,31 @@
+import throttle from 'lodash.throttle';
 
+const LOCALSTORAGE_KEY = 'feedback-form-state';
+const form = document.querySelector('form');
+let formData = {};
+const ls = localStorage;
+
+// to receive data from input
+
+form.addEventListener('input', throttle(onFormInput, 500));
+form.addEventListener('submit', onFormSubmit);
+
+function onFormInput(e) {
+  //   e.preventDefault();
+  formData[e.target.name] = e.target.value;
+  ls.setItem('formData', JSON.stringify(formData));
+  if (ls.getItem('formData')) {
+    formData = JSON.parse(ls.getItem('formData'));
+    for (let key in formData) {
+      form.elements[key].value = formData[key];
+    }
+  }
+}
+
+function onFormSubmit(e) {
+  e.preventDefault();
+
+  console.log('Submitting Form');
+  e.currentTarget.reset();
+  ls.removeItem('formData');
+}
